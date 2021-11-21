b8848296b77f203748be9470c971602f3e16f38d
Ashray <ashraypa2001@gmail.com>
2021-11-21 12:43:04 +0530

    small ones

--- url-shortner/src/app/components/viewstats/viewstats.component.ts
+++ url-shortner/src/app/components/viewstats/viewstats.component.ts
@@ -5,6 +5,7 @@ import {logs} from '../../models'
 import { ChartType, ChartOptions,ChartDataSets   } from 'chart.js';
 import { Label, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip } from 'ng2-charts';
 
+
 @Component({
   selector: 'app-viewstats',
   templateUrl: './viewstats.component.html',
@@ -22,10 +23,28 @@ export class ViewstatsComponent implements OnInit {
   public no_data=false;
   public pieChartLabels1: Label[] = [];
   public pieChartData1: number[] = [];
-
   public pieChartType1: ChartType = 'pie';
   public pieChartLegend1 = true;
   public pieChartPlugins1 = [];
+
+  public pieChartLabels2: Label[] = [];
+  public pieChartData2: number[] = [];
+  public pieChartType2: ChartType = 'pie';
+  public pieChartLegend2 = true;
+  public pieChartPlugins2 = [];
+
+  public pieChartLabels3: Label[] = [];
+  public pieChartData3: number[] = [];
+  public pieChartType3: ChartType = 'pie';
+  public pieChartLegend3 = true;
+  public pieChartPlugins3 = [];
+
+  public barChartLabels: Label[] = [];
+  public barChart2Type: ChartType = 'bar';
+  public barChart2Legend = false;
+  public barChart2Plugins = [];
+  public barChart2Data: number[] = [];
+
   public donutColors1=[
     {
       backgroundColor: [
@@ -37,7 +56,19 @@ export class ViewstatsComponent implements OnInit {
       ]
     }
   ];
-  public pieChartOptions1: ChartOptions = {
+
+  public barChart2Options: ChartOptions = {
+    responsive: true,
+    scales: {
+      yAxes: [{
+          ticks: {
+              beginAtZero:true
+          }
+      }]
+    }
+  };
+
+  public pieChartOptions: ChartOptions = {
     responsive: true,
     legend:{
       position:"top",
@@ -53,6 +84,7 @@ export class ViewstatsComponent implements OnInit {
       this.no_data=true
     }
     else{
+      console.log(this.logs);
       for(var log of this.logs){
         if(!this.pieChartLabels1.includes(log.os)){
           this.pieChartLabels1.push(log.os)
@@ -61,9 +93,31 @@ export class ViewstatsComponent implements OnInit {
         else{
           this.pieChartData1[this.pieChartLabels1.indexOf(log.os)]+=1
         }
+
+        if(!this.pieChartLabels2.includes(log.browser)){
+          this.pieChartLabels2.push(log.browser)
+          this.pieChartData2.push(1)
+        }
+        else{
+          this.pieChartData2[this.pieChartLabels2.indexOf(log.browser)]+=1
+        }
+
+        if(!this.pieChartLabels3.includes(log.deviceType)){
+          this.pieChartLabels3.push(log.deviceType)
+          this.pieChartData3.push(1)
+        }
+        else{
+          this.pieChartData3[this.pieChartLabels3.indexOf(log.deviceType)]+=1
+        }
+
+        if(!this.barChartLabels.includes(log.country)){
+          this.barChartLabels.push(log.country);
+          this.barChart2Data.push(1);
+        }
+        else{
+          this.barChart2Data[this.barChartLabels.indexOf(log.country)] += 1
+        }
       }
-      console.log(this.pieChartLabels1)
-      console.log(this.pieChartData1)
     }
   }
 
